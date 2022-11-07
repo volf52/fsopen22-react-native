e6eddc71c5b861a72b8240c6dc5ba3ebbd0ea296
Arslan <rslnkrmt2552@gmail.com>
2022-11-07 17:56:37 +0500

    ex 10.7

--- rate-repository-app/src/components/nav/AppBar.jsx
+++ rate-repository-app/src/components/nav/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, ScrollView } from "react-native"
 import Constants from "expo-constants"
 
 import AppBarTab from "./AppBarTab"
@@ -18,8 +18,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories" link="/" />
-      <AppBarTab text="Sign in" link="/login" />
+      <ScrollView horizontal>
+        <AppBarTab text="Repositories" link="/" />
+        <AppBarTab text="Sign in" link="/login" />
+      </ScrollView>
     </View>
   )
 }
