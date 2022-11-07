0f88c4b81fb40d9b19118732a63bc98d5095349c
Arslan <rslnkrmt2552@gmail.com>
2022-11-07 18:28:27 +0500

    ex 10.9

--- rate-repository-app/src/components/signin/SignInContainer.jsx
+++ rate-repository-app/src/components/signin/SignInContainer.jsx
@@ -1,4 +1,5 @@
 import { Formik } from "formik"
+import * as yup from "yup"
 
 import SignInForm from "./SignInForm"
 
@@ -7,9 +8,27 @@ const initialValues = {
   password: "",
 }
 
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 character")
+    .max(30, "Username must be at most 30 characters")
+    .required("Username is required"),
+
+  password: yup
+    .string()
+    .min(3, "Password must be at least 3 characters")
+    .max(50, "Password must be at most 50 characters")
+    .required("Password is required"),
+})
+
 const SignInContainer = ({ onSubmit }) => {
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   )
